884ef86a332473837c072a35bb8e9c8e01f32504
userLittleBoy <934673360@qq.com>
2019-11-19 11:48:24 +0800

    hao

--- 艺校/pages/dynamic/index.js
+++ 艺校/pages/dynamic/index.js
@@ -1,30 +1,12 @@
 //index.js
 //获取应用实例
 const app = getApp()
-import url from "../../config/url.js"
+
 Page({
   data: {
-    
+   
   },
-  goDetail(e){
-    //   console.log(e.currentTarget.dataset.id)
-    wx.navigateTo({
-        url: '/pages/detail/detail?con='+JSON.stringify(this.data.list[e.currentTarget.dataset.id])
-    })
-  },
-  onLoad(){
-      wx.request({
-          url: url.list,
-          data: {},
-          method: 'GET',
-          success: (res)=>{
-              this.setData({
-                  list:res.data.msg
-              })
-          }
-      })
-    wx.setNavigationBarTitle({
-        title: '动态'
-    })
+  onLoad: function () {
+   
   }
 })
