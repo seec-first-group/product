183649d3f692a071c4a92d72f881838b7b5517a6
liudidegithub <785708950@qq.com>
2019-11-19 14:26:23 +0800

    asda

--- 艺校/pages/common/details/details.js
+++ 艺校/pages/common/details/details.js
@@ -8,20 +8,19 @@ Page({
 		showModal: false,
 		numm: "1"
 	},
-
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
 	onLoad: function(options) {
 		var id = JSON.parse(options.id);
-		var that = this;
+		console.log(id);
+		let that = this;
 		that.setData({
 			title: id.title,
 			img: id.image,
 			num: id.num,
 			price: id.price,
-			selected: id.selected
+			selected: id.selected,
+			id: id
 		})
+		// console.log(that.data);
 
 	},
 	buy(e) {
@@ -35,22 +34,63 @@ Page({
 		// this.setData({
 		// 	showModal: false
 		// })
-		var id = e.currentTarget.dataset.id;
+		// var id = e.currentTarget.dataset.id;
+		// ?id=" + JSON.stringify(id)
+		var obj = {
+			title: this.data.title,
+			img: this.data.img,
+			numm: this.data.numm,
+			price: this.data.price,
+			selected: this.data.selected,
+			allP: this.data.allP
+		}
+		wx.setStorageSync("key", obj);
+		console.log(obj);
 		wx.navigateTo({
-			url: "/pages/common/buy/buy?id=" + JSON.stringify(id)
+			url: "/pages/common/buy/buy"
 		})
 	},
 	// 禁止屏幕滚动
-	preventTouchMove: function() {},
+	preventTouchMove() {},
 
 	// 弹出层里面的弹窗
-	ok: function() {
+	ok() {
 		this.setData({
 			showModal: false
 		})
 	},
-	asd() {
-		this.numm++;
+	asd(e) {
+		var numm = this.data.numm;
+		numm++;
+		this.setData({
+			numm
+		})
+		this.hna();
+	},
+	jian(e) {
+		var numm = this.data.numm;
+		numm--;
+		if (numm <= 1) {
+			numm = 1
+		}
+		this.setData({
+			numm
+		})
+		this.hna();
+	},
+	onShow(e) {
+
+		this.hna();
+
+	},
+	hna() {
+		var numm = this.data.numm;
+		var pric = this.data.price;
+		console.log(numm);
+		var allP = numm * pric
+		this.setData({
+			allP
+		})
 	}
 
 })
