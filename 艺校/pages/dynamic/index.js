bcd50b75ded1b46f416d48aaff9e270e45bcd71c
userLittleBoy <934673360@qq.com>
2019-11-19 11:19:26 +0800

    ding

--- 艺校/pages/dynamic/index.js
+++ 艺校/pages/dynamic/index.js
@@ -1,12 +1,30 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+import url from "../../config/url.js"
 Page({
   data: {
-   
+    
   },
-  onLoad: function () {
-   
+  goDetail(e){
+    //   console.log(e.currentTarget.dataset.id)
+    wx.navigateTo({
+        url: '/pages/detail/detail?con='+JSON.stringify(this.data.list[e.currentTarget.dataset.id])
+    })
+  },
+  onLoad(){
+      wx.request({
+          url: url.list,
+          data: {},
+          method: 'GET',
+          success: (res)=>{
+              this.setData({
+                  list:res.data.msg
+              })
+          }
+      })
+    wx.setNavigationBarTitle({
+        title: '动态'
+    })
   }
 })
