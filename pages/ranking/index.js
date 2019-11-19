b93f2cfb0968f86233aca4869ee5c674f7fe67fe
userLittleBoy <934673360@qq.com>
2019-11-19 14:03:09 +0800

    aaa

---
+++ pages/ranking/index.js
@@ -0,0 +1,56 @@
+// pages/ranking/index.js
+Page({
+  data: {
+    list:[
+      {
+        imgUrl:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"3"
+      },
+      {
+        imgUrl:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"2"
+      },
+      {
+        imgUrl:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"1"
+      }
+    ],
+    listArr:[
+      {
+        num:1,
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"1"
+      },
+      {
+        num:2,
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"2"
+      },
+      {
+        num:3,
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"1"
+      },
+      {
+        num:4,
+        img:"https://raw.githubusercontent.com/userLittleBoy/img/master/img/8_03.png",
+        name:"林夕",
+        zan:"3"
+      }
+    ]
+  },
+  onLoad: function (options) {
+
+  },
+  
+
+})
